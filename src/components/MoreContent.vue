23dc1413bef37af47e2c715c3bdbc773be611a88
lhkuan <69495276+lhkuan@users.noreply.github.com>
2024-05-07 14:55:23 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="more-content">微信：liduwood </div>
+  <div class="more-content">
+    木韵传千里，板材质非凡。
+    匠心独运处，微信号中求。
+          微信：liduwood </div>
 </template>
 
 <style lang="scss" scoped>
