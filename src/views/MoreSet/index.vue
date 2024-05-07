ae5b9e01d431ea05035ed7028460ca06ef7267fc
lhkuan <69495276+lhkuan@users.noreply.github.com>
2024-05-07 14:27:06 +0800

    Update index.vue

--- src/views/MoreSet/index.vue
+++ src/views/MoreSet/index.vue
@@ -18,7 +18,7 @@
         </div>
         <div class="version">
           <div class="num">v&nbsp;{{ config.version }}</div>
-          <el-tooltip content="Github 源代码仓库" placement="right" :show-arrow="false">
+          <el-tooltip content="Github" placement="right" :show-arrow="false">
             <github-one class="github" theme="outline" size="24" @click="jumpTo(config.github)" />
           </el-tooltip>
         </div>
