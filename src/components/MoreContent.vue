316cef8028ad0c4d5fbb2448add237e240cbd7c9
lhkuan <69495276+lhkuan@users.noreply.github.com>
2024-05-11 09:45:15 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">卖板</div>
+  <div class="more-content">板油诗</div>
   <div class="more-content">板材生涯度春秋，友朋满座共欢聚。</div>
   <div class="more-content">质量严格心中守，价格公道任君求。</div>
   <div class="more-content">信誉如山从不负，他人欠我亦无忧。</div>
